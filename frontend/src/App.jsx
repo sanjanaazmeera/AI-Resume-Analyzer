942d97d13733839ed217b6a45666f99c6cfcb3f1
sanjanaazmeera <sanjanaazmeera7392@gmail.com>
2026-08-12 19:02:25 +0530

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,10 +24,11 @@ function App() {
 
       setLoading(true);
 
-      const res = await axios.post(
-        "http://localhost:5000/api/resume/upload",
-        formData
-      );
+   const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+   const res = await axios.post(
+     `${API_URL}/api/resume/upload`,
+     formData
+   );
 
       setResult(res.data);
 
